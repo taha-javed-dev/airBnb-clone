e3db8ccb67181086d52ced4c93b8341e15da5b5a
taha-javed-dev <taha.javed.dev@gmail.com>
2026-06-08 12:48:26 +0530

    initial commit

---
+++ src/main/java/com/taha/airbnbclone/AirBnbCloneApplication.java
@@ -0,0 +1,13 @@
+package com.taha.airbnbclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirBnbCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirBnbCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taha/airbnbclone/AirBnbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taha.airbnbclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirBnbCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
